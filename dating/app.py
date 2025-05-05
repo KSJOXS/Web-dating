9989a66db6e1067203713e11dd432f782353595f
KSJOXS <your_email@example.com>
2025-05-05 22:49:36 +0700

    save chage project

--- dating/app.py
+++ dating/app.py
@@ -2,26 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
-from models import db
-from views import HomeView, ExploreView, ProfileView, RegisterView, LoginView, LogoutView
+from models import db, User
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Initialize database and login manager
 db.init_app(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
-# Add URL rules for class-based views
-app.add_url_rule('/', view_func=HomeView.as_view('home'))
-app.add_url_rule('/explore', view_func=ExploreView.as_view('explore'))
-app.add_url_rule('/profile', view_func=ProfileView.as_view('profile'))
-app.add_url_rule('/register', view_func=RegisterView.as_view('register'), methods=['GET', 'POST'])
-app.add_url_rule('/login', view_func=LoginView.as_view('login'), methods=['GET', 'POST'])
-app.add_url_rule('/logout', view_func=LogoutView.as_view('logout'))
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Create database if it doesn't exist
+# 🔹 Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login' 
+ # Redirect users to login page if not authenticated
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  
+
+from views import *
+
+if __name__ == '__main__':
     app.run(debug=True)
--- dating/forms.py
+++ dating/forms.py
@@ -1,15 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, FileField, ValidationError, DateField
+from flask_wtf.file import FileAllowed
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+from werkzeug.utils import secure_filename
 
-class RegistrationForm(FlaskForm):
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  # Allowed file extensions
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+ 
+class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+class ProfileForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(max=50)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(max=50)])  # <== Add this
+    gender = SelectField('Gender', choices=[('MALE', 'Male'), ('FEMALE', 'Female'), ('OTHER', 'Other')], validators=[DataRequired()])
+    date_of_birth = DateField('Date of Birth', validators=[DataRequired()], format='%Y-%m-%d')
+    bio = TextAreaField('Bio', validators=[Length(max=500)])
+    profile_picture = FileField('Profile Picture', validators=[FileAllowed(ALLOWED_EXTENSIONS, 'Images only!')])
+    location = StringField('Location', validators=[Length(max=100)])
+    interests = TextAreaField('Interests', validators=[Length(max=500)])
+    submit = SubmitField('Save Profile')
